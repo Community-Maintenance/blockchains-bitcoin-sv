bec253d82179a67a0228e80fa94b9dd095afef3b
Karl Semich <0xloem@gmail.com>
2020-10-29 15:16:43 -0400

    -

---
+++ blocks/upload.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+num=0
+while true
+do
+	blknum="$(printf %05i "$num").dat"
+	if ! [ -e blk"$blknum" ]
+	then
+		break
+	fi
+	git annex add *"$blknum" &&
+	git annex unlock *"$blknum" &&
+	git annex copy *"$blknum" --to=skynet || exit -1
+	while ! git annex fsck -f skynet *"$blknum"
+	do
+		git annex copy *"$blknum" --to=skynet
+	done
+	git commit -m "added $blknum"
+	git annex sync origin
+	git annex unlock .
+	num=$((num+1))
+done
